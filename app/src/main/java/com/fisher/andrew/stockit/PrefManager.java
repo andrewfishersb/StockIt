c9ea06256de0fa25a3a2aa23ee27efe0d6b633dc
Andrew Fisher <andrewfishersb@gmail.com>
2017-04-05 12:26:36 -0700

    added strings and dimentions in the XML files, create a PrefManager class which will open the landing page once

---
+++ app/src/main/java/com/fisher/andrew/stockit/PrefManager.java
@@ -0,0 +1,23 @@
+package com.fisher.andrew.stockit;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by andrewfisher on 4/5/17.
+ */
+
+public class PrefManager {
+    SharedPreferences mPref;
+    SharedPreferences.Editor mEditor;
+    Context mContext;
+
+    //Shared Pref Mode
+    int PRIVATE_MODE = 0;
+
+
+
+
+
+
+}

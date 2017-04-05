aaa2ba9dbab343b678be46696c9044768c509baf
Andrew Fisher <andrewfishersb@gmail.com>
2017-04-05 12:35:11 -0700

    Pref Manager will set and check if this is the first time the app has launched

--- app/src/main/java/com/fisher/andrew/stockit/PrefManager.java
+++ app/src/main/java/com/fisher/andrew/stockit/PrefManager.java
@@ -3,21 +3,31 @@ package com.fisher.andrew.stockit;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-/**
- * Created by andrewfisher on 4/5/17.
- */
 
 public class PrefManager {
     SharedPreferences mPref;
     SharedPreferences.Editor mEditor;
     Context mContext;
 
-    //Shared Pref Mode
     int PRIVATE_MODE = 0;
 
+    private static final String PREF_NAME = "stockit-welcome";
 
+    private static final String FIRST_TIME_LAUNCH = "FirstLaunch";
 
+    public PrefManager(Context context){
+        mContext=context;
+        mPref = mContext.getSharedPreferences(PREF_NAME,PRIVATE_MODE);
+        mEditor = mPref.edit();
+    }
 
+    public void setFirstTimeLaunch(boolean isFirstTime){
+        mEditor.putBoolean(FIRST_TIME_LAUNCH,isFirstTime);
+        mEditor.commit();
+    }
 
+    public boolean isFirstTimeLaunch(){
+        return mPref.getBoolean(FIRST_TIME_LAUNCH,true);
+    }
 
 }
